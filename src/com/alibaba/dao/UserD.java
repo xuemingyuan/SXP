34502d38488f92423739a5b148c0057c5588fea9
xuemingyuan <463417978@qq.com>
2017-04-16 21:44:07 +0800

    learning mybatis

--- src/com/alibaba/dao/UserD.java
+++ src/com/alibaba/dao/UserD.java
@@ -1,14 +1,15 @@
 package com.alibaba.dao;
 
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import com.alibaba.model.User;
 import com.alibaba.util.Constants;
 
-
+@Repository("userDao")
 public interface UserD {
 
-	@Select(Constants.SQL_TEST)
+	@Select(Constants.SQL_QUERY_DEAL_BY_ID)
 	public User findUserByName(String username);
 	
 }
--- src/com/alibaba/service/UserService.java
+++ src/com/alibaba/service/UserService.java
@@ -3,14 +3,14 @@ package com.alibaba.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.dao.UserDao;
+import com.alibaba.dao.UserD;
 import com.alibaba.model.User;
 
 @Service("userService")
 public class UserService {
 
 	@Autowired
-	private UserDao userDao;
+	private UserD userDao;
 	
 	public User findUserByName(String username) {
 		User user = userDao.findUserByName(username);
