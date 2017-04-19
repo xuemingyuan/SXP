494a1f6caab2f2b7040f152bd4e31502d95326c0
xuemingyuan <463417978@qq.com>
2017-04-19 22:29:58 +0800

    add Junit test

---
+++ test/com/alibaba/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.alibaba.service;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:springMybatis-servlet.xml")
+public class UserServiceTest 
+{
+
+	@Resource
+	UserService userService;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindUserByName() {
+		long startTime = System.currentTimeMillis();
+		userService.findUserByName("a");
+		System.out.println(System.currentTimeMillis()-startTime);
+		
+		long startTime2 = System.currentTimeMillis();
+		userService.findUserByName("a");
+		System.out.println(System.currentTimeMillis()-startTime2);
+	}
+
+}
