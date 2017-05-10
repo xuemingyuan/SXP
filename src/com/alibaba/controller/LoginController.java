69d82435fe2887fb1044c892c30231b195843e92
xuemingyuan <463417978@qq.com>
2017-05-10 19:57:30 +0800

    commit

--- src/com/alibaba/controller/LoginController.java
+++ src/com/alibaba/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.alibaba.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
